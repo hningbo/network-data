22005398beaa091e179d9d828ae76023138b775a
rylynn-abo <rylynn_ab@163.com>
2018-06-15 10:54:29 +0800

    extends to 139 mail

---
+++ src/main/java/edu/rylynn/netdata/core/mail/listener/Listener139.java
@@ -0,0 +1,10 @@
+package edu.rylynn.netdata.core.mail.listener;
+
+import org.pcap4j.packet.Packet;
+
+public class Listener139 extends AbstractWebMailListener {
+    @Override
+    public void gotPacket(Packet packet) {
+
+    }
+}
---
+++ src/main/java/edu/rylynn/netdata/core/mail/processor/Processor139.java
@@ -0,0 +1,19 @@
+package edu.rylynn.netdata.core.mail.processor;
+
+import edu.rylynn.netdata.core.mail.listener.AbstractWebMailListener;
+
+public class Processor139 extends AbstractProcessor {
+    public Processor139(AbstractWebMailListener listenerMail) {
+        super(listenerMail);
+    }
+
+    @Override
+    public void sendMailExtract(String content) {
+        return;
+    }
+
+    @Override
+    public void recieveMailExtract(String content) {
+        return;
+    }
+}
